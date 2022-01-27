8ee3d6ce17e2a274d400305cc556de3ba6c591f6
Jason Sandlin <jasonsa@microsoft.com>
2022-01-27 13:05:35 -0800

    Fixing gen script

--- pf-gensdk.cmd
+++ pf-gensdk.cmd
@@ -4,5 +4,5 @@ set PF_ROOT=%SCRIPT_PATH%..\..\..
 call %SCRIPT_PATH%\pf-backupandclean.cmd
 pushd .
 cd %PF_ROOT%\SDKGenerator\
-call ts-node generate.js -destpath %PF_ROOT%\sdks\XPlatCSdk -apiSpecGitUrl -buildIdentifier XPlatCSdk_manual
+call ts-node generate.js -destpath %PF_ROOT%\sdks\PlayFabCoreCSdk -apiSpecGitUrl -buildIdentifier PlayFabCoreCSdk_manual
 popd
