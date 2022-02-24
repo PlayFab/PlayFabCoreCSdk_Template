32266620312191bbd5dac46de85a1db5dc30192c
Jason Sandlin <jasonsa@microsoft.com>
2022-02-23 16:20:58 -0800

    1

--- pf-gensdk.cmd
+++ pf-gensdk.cmd
@@ -4,5 +4,7 @@ set PF_ROOT=%SCRIPT_PATH%..\..\..
 call %SCRIPT_PATH%\pf-backupandclean.cmd
 pushd .
 cd %PF_ROOT%\SDKGenerator\
-call ts-node generate.js -destpath %PF_ROOT%\sdks\PlayFabCoreCSdk -apiSpecGitUrl -buildIdentifier PlayFabCoreCSdk_manual
+call ts-node generate.js -destpath %PF_ROOT%\sdks\PlayFabCoreCSdk -apiSpecPath C:\git\pf\SDKGenerator\privateTemplates\PlayFabCoreCSdk_Template\CeruleanSwagger -buildIdentifier PlayFabCoreCSdk_manual
 popd
+
+
