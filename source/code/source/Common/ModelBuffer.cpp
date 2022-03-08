1d6176592c04604700e666507a133e3229f7bc87
Thomas Culotta <tculotta@microsoft.com>
2022-03-07 16:22:30 -0800

    CopyTo error fixed and last AccountManagement test passing

--- source/code/source/Common/ModelBuffer.cpp
+++ source/code/source/Common/ModelBuffer.cpp
@@ -30,7 +30,14 @@ Result<const char*> ModelBuffer::CopyTo(const char* input)
         // Copy
         auto outputPtr = allocResult.ExtractPayload();
 #if HC_PLATFORM_IS_MICROSOFT
-        return strcpy_s(outputPtr, bytesNeeded, input);
+        auto error = strcpy_s(outputPtr, bytesNeeded, input);
+
+        if (error)
+        {
+            return E_FAIL;
+        }
+
+        return outputPtr;
 #else
         return std::strcpy(outputPtr, input);
 #endif
